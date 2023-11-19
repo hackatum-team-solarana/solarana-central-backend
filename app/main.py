2f9078fb48d6de11d0b1878a46df9af4bcb82c94
Simon Okutan <90206664+Nomiez@users.noreply.github.com>
2023-11-19 04:54:52 +0100

    Add CORS settings

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
 from app.routes import marketplace_route
@@ -8,6 +9,14 @@ from app.models import offers
 offers.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(marketplace_route.router)
 
